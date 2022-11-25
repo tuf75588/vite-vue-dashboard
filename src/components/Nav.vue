d8f2292846b4a2bf6cc050133d511e1fd4b70d10
Andrew Davis <atd285@gmail.com>
2022-11-25 18:11:35 -0500

    navbar first draft

---
+++ src/components/Nav.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <nav class="nav">
+    <h1>{{msg}}</h1>
+    <p>Last updated at: {{new Date().toString()}}</p>
+  </nav>
+</template>
+
+<style scoped>
+.nav  {
+  text-align: center;
+  padding: 2em;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: #fefefe;
+}
+.nav > p {
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
